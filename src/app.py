a30cf037817dd3f442d5e2e46ec8f8fe3089914e
Manueldlcasencio <manuel.dlcasencio@gmail.com>
2023-04-13 14:39:19 +0000

    Proyecto terminado

--- src/app.py
+++ src/app.py
@@ -12,12 +12,20 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+# JWT - Token
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 7200
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
